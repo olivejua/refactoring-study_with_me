eb338419de28de545ce7b0be7fde75c9bb377c78
olivejuwa <tmfrl4710@gmail.com>
2021-04-20 15:07:02 +0900

    질문게시판 repository - (JpaRepository, querydsl custom repository 같이 사용하는 것 시도)

--- src/main/java/com/olivejua/study/repository/board/QuestionRepository.java
+++ src/main/java/com/olivejua/study/repository/board/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.olivejua.study.repository.board;
 
 import com.olivejua.study.domain.board.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
+    Page<Question> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
