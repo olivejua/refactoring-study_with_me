6d3b79be676bef651da23695f8fdd10cb0967cfe
olivejua <tmfrl4710@gmail.com>
2021-05-12 20:54:10 +0900

    [service] 질문게시판 목록 가져오기, 검색 기능 구현, 테스트

--- src/test/java/com/olivejua/study/repository/board/QuestionRepositoryTest.java
+++ src/test/java/com/olivejua/study/repository/board/QuestionRepositoryTest.java
@@ -5,11 +5,24 @@ import com.olivejua.study.domain.board.Question;
 import com.olivejua.study.repository.UserRepository;
 import com.olivejua.study.sampleData.SampleQuestion;
 import com.olivejua.study.sampleData.SampleUser;
+import com.olivejua.study.web.dto.board.question.PostListResponseDto;
+import com.olivejua.study.web.dto.board.search.SearchDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class QuestionRepositoryTest {
@@ -20,9 +33,12 @@ class QuestionRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
     @DisplayName("Question - 저장")
-    public void save() {
+    void save() {
         User writer = SampleUser.create();
         userRepository.save(writer);
 
@@ -31,6 +47,50 @@ class QuestionRepositoryTest {
 
         Question findPost = questionRepository.findAll().get(0);
 
-        Assertions.assertEquals(post, findPost);
+        assertEquals(post, findPost);
+    }
+
+    @Test
+    void list() {
+        User writer = SampleUser.create();
+        userRepository.save(writer);
+
+        for (int i=0; i<100; i++) {
+            questionRepository.save(SampleQuestion.create(writer));
+        }
+
+        em.flush();
+        em.clear();
+
+
+        PageRequest paging = PageRequest.of(0, 10, Sort.Direction.ASC, "POST_ID");
+
+        Page<PostListResponseDto> posts = questionRepository.list(paging);
+        assertEquals(10, posts.getNumberOfElements());
+    }
+
+    @Test
+    void search() {
+        User writer = SampleUser.create();
+        userRepository.save(writer);
+
+        String[] titles = new String[5];
+        for (int i=0; i< titles.length; i++) {
+            titles[i] = "제목"+(i+1);
+        }
+
+        for (int i=0; i<20; i++) {
+            questionRepository.save(Question.savePost(
+                    writer,
+                    titles[i%titles.length],
+                    "샘플 내용"
+            ));
+        }
+
+        SearchDto searchDto = new SearchDto("TITLE", "제목3");
+        PageRequest paging = PageRequest.of(0, 10, Sort.Direction.ASC, "POST_ID");
+        Page<PostListResponseDto> savedPosts = questionRepository.search(searchDto, paging);
+
+        assertEquals(4, savedPosts.getTotalElements());
     }
 }
