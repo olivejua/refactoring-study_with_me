8470fd7c3a694f8384b2ff0a15eed06096cbc37a
olivejuwa <tmfrl4710@gmail.com>
2021-05-20 18:57:16 +0900

    [service, queryRepository] StudyRecruitment 페이징 최적화

--- src/main/java/com/olivejua/study/repository/board/StudyRecruitmentQueryRepository.java
+++ src/main/java/com/olivejua/study/repository/board/StudyRecruitmentQueryRepository.java
@@ -1,20 +1,24 @@
 package com.olivejua.study.repository.board;
 
+import com.olivejua.study.domain.board.StudyRecruitment;
 import com.olivejua.study.web.dto.board.search.SearchDto;
 import com.olivejua.study.web.dto.board.search.SearchType;
 import com.olivejua.study.web.dto.board.study.PostListResponseDto;
-import com.querydsl.core.QueryResults;
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import static com.olivejua.study.domain.QComment.comment;
+import static com.olivejua.study.domain.QUser.user;
 import static com.olivejua.study.domain.board.QStudyRecruitment.studyRecruitment;
 import static com.olivejua.study.domain.board.QTechStack.techStack;
 
@@ -25,67 +29,78 @@ public class StudyRecruitmentQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     public Page<PostListResponseDto> list(Pageable pageable) {
-        QueryResults<PostListResponseDto> results = queryFactory
-                .selectDistinct(Projections.constructor(PostListResponseDto.class,
-                        studyRecruitment.id,
-                        studyRecruitment.title,
-                        studyRecruitment.writer.name,
-                        studyRecruitment.viewCount,
-                        studyRecruitment.comment.size()))
-                .from(studyRecruitment)
-                .join(studyRecruitment.techStack, techStack)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetchResults();
+        List<PostListResponseDto> content = selectPosts(pageable);
 
-        List<PostListResponseDto> content = results.getResults();
-        long total = results.getTotal();
+        JPAQuery<StudyRecruitment> countQuery =
+                    queryFactory
+                        .selectFrom(studyRecruitment);
 
-        return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
     }
 
     public Page<PostListResponseDto> search(SearchDto cond, Pageable pageable) {
-        QueryResults<PostListResponseDto> results = queryFactory
-                .select(Projections.constructor(PostListResponseDto.class,
-                        studyRecruitment.id,
-                        studyRecruitment.title,
-                        studyRecruitment.writer.name,
-                        studyRecruitment.viewCount,
-                        studyRecruitment.comment.size()))
-                .from(studyRecruitment)
-                .join(studyRecruitment.techStack, techStack)
-                .where(titleEq(cond),
-                        placeEq(cond),
-                        explanationEq(cond),
-                        techStackContains(cond))
+        List<PostListResponseDto> content = selectPosts(pageable, cond);
+        JPAQuery<StudyRecruitment> countQuery = queryFactory
+                .selectFrom(studyRecruitment)
+                .where(allEq(cond));
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+    }
+
+    private List<PostListResponseDto> selectPosts(Pageable pageable) {
+        return selectPosts(pageable, null);
+    }
+
+    private List<PostListResponseDto> selectPosts(Pageable pageable, SearchDto cond) {
+        List<StudyRecruitment> entities = queryFactory
+                .selectFrom(studyRecruitment)
+                .innerJoin(studyRecruitment.writer, user)
+                .leftJoin(studyRecruitment.techStack, techStack)
+                .leftJoin(studyRecruitment.comment, comment)
+                .fetchJoin()
+                .where(cond == null ? null : allEq(cond))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetchResults();
+                .fetch();
 
-        List<PostListResponseDto> content = results.getResults();
-        long total = results.getTotal();
-
-        return new PageImpl<>(content, pageable, total);
+        return toPostListResponseDtos(entities);
     }
 
-
-    private BooleanExpression titleEq(SearchDto searchDto) {
-        return searchDto.getSearchType() == SearchType.TITLE && searchDto.getKeyword() != null
-                ? studyRecruitment.title.contains(searchDto.getKeyword()) : null;
+    private List<PostListResponseDto> toPostListResponseDtos(List<StudyRecruitment> entities) {
+        return entities.stream()
+                .map(entity -> new PostListResponseDto(
+                        entity.getId(),
+                        entity.getTitle(),
+                        entity.getWriter().getName(),
+                        entity.getViewCount(),
+                        entity.getComment().size()
+                )).collect(Collectors.toList());
     }
 
-    private BooleanExpression placeEq(SearchDto searchDto) {
-        return searchDto.getSearchType() == SearchType.PLACE && searchDto.getKeyword() != null
-                ? studyRecruitment.condition.place.contains(searchDto.getKeyword()) : null;
+    private BooleanExpression allEq(SearchDto cond) {
+        return Objects.requireNonNull(titleEq(cond))
+                .and(placeEq(cond))
+                .and(explanationEq(cond))
+                .and(techStackContains(cond));
     }
 
-    private BooleanExpression explanationEq(SearchDto searchDto) {
-        return searchDto.getSearchType() == SearchType.EXPLANATION && searchDto.getKeyword() != null
-                ? studyRecruitment.condition.explanation.contains(searchDto.getKeyword()) : null;
+    private BooleanExpression titleEq(SearchDto cond) {
+        return cond.getSearchType() == SearchType.TITLE && cond.getKeyword() != null
+                ? studyRecruitment.title.contains(cond.getKeyword()) : null;
     }
 
-    private BooleanExpression techStackContains(SearchDto searchDto) {
-        return searchDto.getSearchType() == SearchType.TECH_STACK && searchDto.getKeyword() != null
-                ? techStack.element.contains(searchDto.getKeyword()) : null;
+    private BooleanExpression placeEq(SearchDto cond) {
+        return cond.getSearchType() == SearchType.PLACE && cond.getKeyword() != null
+                ? studyRecruitment.condition.place.contains(cond.getKeyword()) : null;
+    }
+
+    private BooleanExpression explanationEq(SearchDto cond) {
+        return cond.getSearchType() == SearchType.EXPLANATION && cond.getKeyword() != null
+                ? studyRecruitment.condition.explanation.contains(cond.getKeyword()) : null;
+    }
+
+    private BooleanExpression techStackContains(SearchDto cond) {
+        return cond.getSearchType() == SearchType.TECH_STACK && cond.getKeyword() != null
+                ? techStack.element.contains(cond.getKeyword()) : null;
     }
 }
