c82aa7f6e82e214c1a4d9d205a0465a3b198f431
olivejua <tmfrl4710@gmail.com>
2021-06-08 23:03:20 +0900

    exception, exception handler 추가

---
+++ src/main/java/com/olivejua/study/exception/NotExistingUserException.java
@@ -0,0 +1,10 @@
+package com.olivejua.study.exception;
+
+public class NotExistingUserException extends RuntimeException {
+
+    private static final String NOT_EXISTING_USER_MESSAGE = "존재하지 않는 유저입니다.";
+
+    public NotExistingUserException() {
+        super(NOT_EXISTING_USER_MESSAGE);
+    }
+}
