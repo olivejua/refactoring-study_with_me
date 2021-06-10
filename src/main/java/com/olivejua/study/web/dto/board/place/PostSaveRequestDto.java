4c682990983b345f621b03a7fb5fc248dda68029
olivejua <tmfrl4710@gmail.com>
2021-06-10 23:05:52 +0900

    requestDto 기본생성자 추가

--- src/main/java/com/olivejua/study/web/dto/board/place/PostSaveRequestDto.java
+++ src/main/java/com/olivejua/study/web/dto/board/place/PostSaveRequestDto.java
@@ -1,10 +1,12 @@
 package com.olivejua.study.web.dto.board.place;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class PostSaveRequestDto {
     private String title;
     private String address;
@@ -12,21 +14,14 @@ public class PostSaveRequestDto {
     private List<String> links;
     private String thumbnailPath;
     private String content;
-    private int likeCount;
-    private int dislikeCount;
-    private int viewCount;
 
     public PostSaveRequestDto(String title, String address, String addressDetail,
-                              List<String> links, String thumbnailPath, String content,
-                              int likeCount, int dislikeCount, int viewCount) {
+                              List<String> links, String thumbnailPath, String content) {
         this.title = title;
         this.address = address;
         this.addressDetail = addressDetail;
         this.links = links;
         this.thumbnailPath = thumbnailPath;
         this.content = content;
-        this.likeCount = likeCount;
-        this.dislikeCount = dislikeCount;
-        this.viewCount = viewCount;
     }
 }
