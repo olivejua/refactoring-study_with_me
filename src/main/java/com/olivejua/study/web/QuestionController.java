a69d852828e77d23cb7db86787957f3381eec7f8
olivejuwa <tmfrl4710@gmail.com>
2021-06-10 20:17:00 +0900

    [controller] question CRUD 구현 및 테스트

--- src/main/java/com/olivejua/study/web/QuestionController.java
+++ src/main/java/com/olivejua/study/web/QuestionController.java
@@ -2,14 +2,16 @@ package com.olivejua.study.web;
 
 import com.olivejua.study.config.auth.LoginUser;
 import com.olivejua.study.config.auth.dto.SessionUser;
+import com.olivejua.study.service.QuestionService;
+import com.olivejua.study.web.dto.PageDto;
 import com.olivejua.study.web.dto.board.question.PostReadResponseDto;
+import com.olivejua.study.web.dto.board.question.PostSaveRequestDto;
+import com.olivejua.study.web.dto.board.question.PostUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,8 +20,28 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class QuestionController {
 
+    private final QuestionService questionService;
+
     @GetMapping("/{postId}")
-    public ResponseEntity<PostReadResponseDto> read(@PathVariable Long postId, @LoginUser SessionUser user, HttpServletRequest request) {
-        return null;
+    public PostReadResponseDto read(@PathVariable Long postId, PageDto pageInfo) {
+        PostReadResponseDto responseDto = questionService.read(postId);
+        responseDto.savePageInfo(pageInfo);
+
+        return responseDto;
+    }
+
+    @PostMapping
+    public Long post(@RequestBody PostSaveRequestDto requestDto, @LoginUser SessionUser user) {
+        return questionService.post(requestDto, user.toEntity());
+    }
+
+    @PutMapping("/{postId}")
+    public Long update(@PathVariable Long postId, @RequestBody PostUpdateRequestDto requestDto) {
+        return questionService.update(postId, requestDto);
+    }
+
+    @DeleteMapping("/{postId}")
+    public Long delete(@PathVariable Long postId) {
+        return questionService.delete(postId);
     }
 }
---
+++ src/test/java/com/olivejua/study/web/QuestionControllerTest.java
@@ -0,0 +1,151 @@
+package com.olivejua.study.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.olivejua.study.config.auth.dto.SessionUser;
+import com.olivejua.study.domain.Role;
+import com.olivejua.study.domain.User;
+import com.olivejua.study.domain.board.Question;
+import com.olivejua.study.service.QuestionService;
+import com.olivejua.study.web.dto.board.question.PostReadResponseDto;
+import com.olivejua.study.web.dto.board.question.PostSaveRequestDto;
+import com.olivejua.study.web.dto.board.question.PostUpdateRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.persistence.EntityManager;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class QuestionControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private QuestionService questionService;
+
+    private MockHttpSession httpSession;
+
+    @BeforeEach
+    void setup(WebApplicationContext webApplicationContext) {
+        this.mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+
+        httpSession = new MockHttpSession();
+    }
+
+    @Test
+    @DisplayName("[Question] 게시글을 하나 조회한다")
+    public void readTest() throws Exception {
+        User writer = User.createUser(
+                "user1",
+                "user1@gmail.com",
+                Role.USER,
+                "google");
+        em.persist(writer);
+
+        Question post = Question.savePost(
+                writer, "sample title1", "sample content1");
+        em.persist(post);
+
+        when(questionService.read(anyLong())).thenReturn(new PostReadResponseDto(post));
+
+        mvc.perform(get("/question/"+post.getId())
+                .param("page", String.valueOf(0)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").isNotEmpty())
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect(jsonPath("$.viewCount").isNotEmpty())
+                .andExpect(jsonPath("$.comments").isArray())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("[Question] 게시글을 작성한다")
+    public void postTest() throws Exception {
+        PostSaveRequestDto requestDto =
+                new PostSaveRequestDto("sample title1 in a new post", "sample content1 in a new post");
+
+        User user = User.createUser(
+                "current user", "user1@gmail.com", Role.USER, "google");
+        em.persist(user);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        mvc.perform(post("/question")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(requestDto))
+                .session(httpSession))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("[Question] 게시글을 수정한다")
+    public void updateTest() throws Exception {
+        //given
+        User user = User.createUser(
+                "current user", "user1@gmail.com", Role.USER, "google");
+        em.persist(user);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        Question post = Question.savePost(user, "sample title", "sample content");
+        em.persist(post);
+
+        //when
+        PostUpdateRequestDto requestDto =
+                new PostUpdateRequestDto("updated title", "updated content");
+
+        //then
+        mvc.perform(put("/question/"+post.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("[Question] 게시글을 삭제한다")
+    public void deleteTest() throws Exception {
+        //given
+        User user = User.createUser(
+                "current user", "user1@gmail.com", Role.USER, "google");
+        em.persist(user);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        //when
+        when(questionService.delete(anyLong())).thenReturn(anyLong());
+
+        //then
+        mvc.perform(delete("/question/"+1L))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(questionService, atLeastOnce()).delete(anyLong());
+    }
+}
