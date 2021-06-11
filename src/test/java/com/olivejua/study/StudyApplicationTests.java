63d01af7fc7c55de38873175717a7eb97b73d8f8
olivejua <tmfrl4710@gmail.com>
2021-06-12 00:00:22 +0900

    application.yml 업데이트
    profile.include 추가 oauth, real-db

--- src/test/java/com/olivejua/study/StudyApplicationTests.java
+++ src/test/java/com/olivejua/study/StudyApplicationTests.java
@@ -2,12 +2,26 @@ package com.olivejua.study;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.parameters.P;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class StudyApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		List<String> list = new ArrayList<>();
+		if (list.isEmpty()) {
+			System.out.println("this list is empty.");
+		} else {
+			System.out.println("this list is not empty.");
+		}
 	}
 
 }
