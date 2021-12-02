88e93c83619bcd6c295feae23c332cd2573b4912
olivejua <tmfrl4710@gmail.com>
2021-12-02 13:30:50 +0900

    feat: [#7] UrlPaths class 생성
    - 모든 Url 상수화하기 위한 목적

---
+++ src/main/java/com/olivejua/study/utils/UrlPaths.java
@@ -0,0 +1,47 @@
+package com.olivejua.study.utils;
+
+import java.net.URI;
+
+public abstract class UrlPaths {
+	private static final String SEP = "/";
+
+	public static final String USERS = SEP + "users";
+
+	public static final String POSTS = SEP + "posts";
+	public static final String STUDY = POSTS + SEP + "study";
+	public static final String QUESTIONS = POSTS + SEP + "questions";
+	public static final String PLACES = POSTS + SEP + "places";
+
+	public static final String COMMENTS = SEP + "comments";
+	public static final String REPLIES = SEP + "replies";
+
+	public static class Users {
+		private static final String COMMON = USERS + SEP;
+
+		public static final String SIGN_UP = COMMON + "signup";
+		public static final String NAME = COMMON + "name";
+		public static final String NAME_LIST = COMMON + "list";
+
+		public static URI createUser(Long id) {
+			return URI.create(String.format("%s%d", USERS +SEP, id));
+		}
+	}
+
+	public static class Study {
+		private static final String COMMON = STUDY + SEP;
+
+		public static final String LIST = COMMON + "list";
+	}
+
+	public static class Questions {
+		private static final String COMMON = QUESTIONS + SEP;
+
+		public static  final String LIST = COMMON + "list";
+	}
+
+	public static class Places {
+		private static final String COMMON = PLACES + SEP;
+
+		public static final String LIST = COMMON + "list";
+	}
+}
