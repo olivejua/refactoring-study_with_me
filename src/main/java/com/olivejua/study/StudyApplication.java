2430e4d2d70f1aa493b293a3820aa6eb0c8a6d5e
olivejua <45840556+olivejua@users.noreply.github.com>
2021-12-05 16:00:00 +0900

    refactor: [#8] 재구성된 엔티티와 호환을 위해 잠시 이전 코드 제거

--- src/main/java/com/olivejua/study/StudyApplication.java
+++ src/main/java/com/olivejua/study/StudyApplication.java
@@ -1,22 +1,12 @@
 package com.olivejua.study;
 
-import com.olivejua.study.config.auth.SessionListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpSessionListener;
 
 @SpringBootApplication
 public class StudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
-	}
-
-	@Bean
-	public HttpSessionListener httpSessionListener() {
-		return new SessionListener();
 	}
 }
--- src/main/java/com/olivejua/study/repository/ReplyRepository.java
+++ src/main/java/com/olivejua/study/repository/ReplyRepository.java
@@ -1,9 +1,7 @@
 package com.olivejua.study.repository;
 
 import com.olivejua.study.domain.Reply;
-import com.olivejua.study.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-    public void deleteRepliesByPost(Post post);
 }
