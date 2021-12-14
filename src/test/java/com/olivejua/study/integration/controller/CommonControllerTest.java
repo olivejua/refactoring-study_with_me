20ca225de03d1d59124747733c4295d3b0a2862c
olivejua <tmfrl4710@gmail.com>
2021-12-14 23:15:49 +0900

    test: [#11] 테스트 CommonControllerTest RestDocs 설정 추가

--- src/test/java/com/olivejua/study/integration/controller/CommonControllerTest.java
+++ src/test/java/com/olivejua/study/integration/controller/CommonControllerTest.java
@@ -1,11 +1,19 @@
 package com.olivejua.study.integration.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.olivejua.study.auth.JwtTokenProvider;
+import com.olivejua.study.common.docs.RestDocsConfiguration;
 import com.olivejua.study.integration.IntegrationTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Import({
+        RestDocsConfiguration.class
+})
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 public class CommonControllerTest extends IntegrationTest {
 
@@ -14,4 +22,11 @@ public class CommonControllerTest extends IntegrationTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected JwtTokenProvider jwtTokenProvider;
+
+    protected String makeAccessToken(Long userId) {
+        return "Bearer " + jwtTokenProvider.createTokenForUser(userId);
+    }
 }
