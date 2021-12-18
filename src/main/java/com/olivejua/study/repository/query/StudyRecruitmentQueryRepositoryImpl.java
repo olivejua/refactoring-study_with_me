eab20e03c2a4d31f9c9c004cea1cc0010afdb0a3
olivejua <45840556+olivejua@users.noreply.github.com>
2021-12-18 16:58:44 +0900

    refactor: [#13] 스터디 모집 게시글 목록 조회 쿼리 메소드 분리

--- src/main/java/com/olivejua/study/repository/query/StudyRecruitmentQueryRepositoryImpl.java
+++ src/main/java/com/olivejua/study/repository/query/StudyRecruitmentQueryRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.olivejua.study.repository.query;
 
 import com.olivejua.study.domain.studyRecruitment.StudyRecruitment;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.olivejua.study.domain.studyRecruitment.QStudyRecruitment.studyRecruitment;
@@ -22,17 +24,26 @@ public class StudyRecruitmentQueryRepositoryImpl implements StudyRecruitmentQuer
 
     @Override
     public Page<StudyRecruitment> findPosts(Pageable pageable) {
-        JPAQuery<StudyRecruitment> query = queryFactory
-                .selectFrom(studyRecruitment)
-                .join(studyRecruitment.author, user)
-                .leftJoin(studyRecruitment.techs.techs, tech);
+        JPAQuery<StudyRecruitment> query = getPostsSelectQuery();
 
         List<StudyRecruitment> content = query
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .orderBy(studyRecruitment.createdDate.desc())
+                .orderBy(createdDateDesc())
                 .fetch();
 
         return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
+    }
+
+    private JPAQuery<StudyRecruitment> getPostsSelectQuery() {
+        return queryFactory
+                .selectFrom(studyRecruitment)
+                .join(studyRecruitment.author, user).fetchJoin()
+                .leftJoin(studyRecruitment.techs.techs, tech).fetchJoin()
+                .distinct();
+    }
+
+    private OrderSpecifier<LocalDateTime> createdDateDesc() {
+        return studyRecruitment.createdDate.desc();
     }
 }
